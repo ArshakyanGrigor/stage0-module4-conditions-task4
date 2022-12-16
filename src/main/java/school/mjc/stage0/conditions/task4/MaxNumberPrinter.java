d72c2b70177dcae7780f4ad8ac330e0db3b4a07f
ArshakyanGrigor <grigor.arshakyan1994@mail.ru>
2022-12-17 01:09:05 +0400

    Updated files

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int max = first;
+        if (second >= first && second >= third) {
+            max = second;
+        } else if (third >= first) {
+            max = third;
+        }
+        System.out.println(max);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryGreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+        int max = first;
+        max = second > first ? second : first;
+        System.out.println(max);
     }
 }
